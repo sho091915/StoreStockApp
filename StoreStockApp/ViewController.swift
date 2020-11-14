b4eef42d5da75f00ce19025a67d501ebd8d19cd7
sho091915 <c0115162b3@edu.teu.ac.jp>
2020-11-14 14:02:53 +0900

    Initial Commit

---
+++ StoreStockApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreStockApp
+//
+//  Created by 佐藤翔馬 on 2020/11/14.
+//  Copyright © 2020 Shoma Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
